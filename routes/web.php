8123fd5f8e729ee49fceef37ace383a672636cdf
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-10-13 18:21:47 -0300

    CRM-2:  be able to login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Welcome::class);
+Route::get('/', Welcome::class)->name('dashboard');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => auth()->logout())->name('auth.logout');
--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Auth\Login;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('should render the component', function () {
@@ -9,5 +10,18 @@ it('should render the component', function () {
 });
 
 it('should be able to login', function () {
+    $user = User::factory()->create([
+        'email'    => 'joe@doe.com',
+        'password' => 'password',
+    ]);
 
+    Livewire::test(Login::class)
+            ->set('email', 'joe@doe.com')
+            ->set('password', 'password')
+            ->call('tryToLogin')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('dashboard'));
+
+    expect(auth()->user()->id)->toEqual($user->id);
+    expect(auth()->check())->toBeTrue();
 });
