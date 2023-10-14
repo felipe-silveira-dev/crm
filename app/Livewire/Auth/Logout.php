6aba66cd346b61fe24926f713616f9c57cc9e669
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-10-14 17:56:38 -0300

    CRM-3: frontend logout button

--- app/Livewire/Auth/Logout.php
+++ app/Livewire/Auth/Logout.php
@@ -6,9 +6,11 @@ use Livewire\Component;
 
 class Logout extends Component
 {
-    public function render()
+    public function render(): string
     {
-        return view('livewire.auth.logout');
+        return <<<BLADE
+            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" wire:click='logout' />
+        BLADE;
     }
 
     public function logout(): void
