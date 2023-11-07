1416f16221d883eb0787fa6bad84beff91c982f7
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-11-06 21:00:11 -0300

    CRM-6: show user

---
+++ resources/views/livewire/admin/users/show.blade.php
@@ -0,0 +1,12 @@
+<div>
+    @isset($this->user)
+        {{ $this->user->name }}
+        <br>
+        {{ $this->user->email }}
+        <br>
+        {{ $this->user->created_at->format('d/m/Y H:i') }}
+        <br>
+        {{ $this->user->updated_at->format('d/m/Y H:i') }}
+        <br>
+    @endisset
+</div>
