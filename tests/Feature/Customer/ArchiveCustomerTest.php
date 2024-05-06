72f7a8042d75e9f388fa9cbd65fd95d852d86182
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-05-06 07:32:45 -0300

    CRM-17: archive component

---
+++ tests/Feature/Customer/ArchiveCustomerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Livewire\Customers;
+use App\Models\{Customer, User};
+
+use Livewire\Livewire;
+
+use function Pest\Laravel\{actingAs, assertSoftDeleted};
+
+it('should be able to archive a customer', function () {
+    $customer = Customer::factory()->create();
+    $user     = User::factory()->create();
+
+    actingAs($user);
+
+    Livewire::test(Customers\Archive::class)
+        ->set('customer', $customer)
+        ->call('archive');
+
+    assertSoftDeleted('customers', [
+        'id' => $customer->id,
+    ]);
+});
