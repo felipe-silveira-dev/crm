33e69625ca00e1c1eedd1bf8a1b49091a9b37ba8
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-05-20 12:10:04 -0300

    crm-22: atualizando ordenação das oportunidades

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Opportunities;
 use App\Models\Opportunity;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -22,18 +23,38 @@ class Board extends Component
     public function opportunities(): Collection
     {
         return Opportunity::query()
-            ->orderByRaw("
-                    case
-                        when status = 'open' then 1
-                        when status = 'won' then 2
-                        when status = 'lost' then 3
-                    end
-                ")
+            // ->orderByRaw("
+            //         case
+            //             when status = 'open' then 1
+            //             when status = 'won' then 2
+            //             when status = 'lost' then 3
+            //         end
+            //     ")
+            ->orderByRaw("field(status, 'open', 'won', 'lost')")
+            ->orderBy('sort_order')
             ->get();
     }
 
-    public function updateOpportunities($data)
+    public function updateOpportunities(array $data)
     {
+        $order = collect();
 
+        foreach($data as $group) {
+            $order->push(
+                collect($group['items'])
+                    ->map(fn ($i) => $i['value'])
+                    ->join(',')
+            );
+        }
+
+        $sortOrder = $order->join(',');
+        $open      = explode(',', $order[0]);
+        $won       = explode(',', $order[1]);
+        $lost      = explode(',', $order[2]);
+
+        DB::table('opportunities')->whereIn('id', $open)->update(['status' => 'open']);
+        DB::table('opportunities')->whereIn('id', $won)->update(['status' => 'won']);
+        DB::table('opportunities')->whereIn('id', $lost)->update(['status' => 'lost']);
+        DB::table('opportunities')->update(['sort_order' => DB::raw("field(id, $sortOrder)")]);
     }
 }
