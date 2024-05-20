02782e0372de82b3a65d9b80cb6a97b5d0c4dd69
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-05-20 08:40:40 -0300

    crm-22: get opportunity

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -2,13 +2,27 @@
 
 namespace App\Livewire\Opportunities;
 
+use App\Models\Opportunity;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
+/**
+ * @property-read Collection|Opportunity[] $opportunities
+ */
 class Board extends Component
 {
     public function render(): View
     {
         return view('livewire.opportunities.board');
+    }
+
+    #[Computed]
+    public function opportunities(): Collection
+    {
+        return Opportunity::query()
+            ->orderBy('created_at', 'desc')
+            ->get();
     }
 }
