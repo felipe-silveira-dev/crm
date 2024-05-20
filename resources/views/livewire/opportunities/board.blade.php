2461da593949b8ed37f863b864d6807a533320d9
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-05-20 09:56:32 -0300

    crm-22: installing livewire sortablejs

--- resources/views/livewire/opportunities/board.blade.php
+++ resources/views/livewire/opportunities/board.blade.php
@@ -1,6 +1,6 @@
-<div class=" p-2 grid grid-cols-3 gap-4 h-full">
+<div class=" p-2 grid grid-cols-3 gap-4 h-full" wire:sortable-group="updateOpportunities">
     @foreach ($this->opportunities->groupBy('status') as $status => $items)
-        <div class="bg-base-200 p-2">
+        <div wire:key="group-{{ $status }}" class="bg-base-200 p-2">
             <x-header
                 title="{{ $status }}"
                 size="text-xl"
@@ -8,11 +8,23 @@
                 subtitle="Total {{ $items->count() }} Opportunities"
                 separator progress-indcator
             />
-            <div class="space-y-2 py-2 px-2">
+            <ul
+                wire:sortable-group.item-group="{{ $status }}"
+                wire:sortable-group.options="{ animation: 100 }"
+                class="space-y-2 px-2"
+            >
                 @foreach ($items as $item)
-                    <x-card>{{ $item->title }}</x-card>
+                    <li
+                        wire:sortable-group.item="{{ $item->id }}"
+                        wire:key="item-{{ $item->id }}"
+                        wire:sortable-group.handle
+                    >
+                        <x-card class="bg-base-100 p-2 rounded-md shadow-md cursor-grab">
+                            {{ $item->title }}
+                        </x-card>
+                    </li>
                 @endforeach
-            </div>
+            </ul>
         </div>
     @endforeach
 </div>
